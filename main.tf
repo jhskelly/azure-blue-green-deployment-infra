f561d09a5360ed1b03fb72697f5a6e0e86113fc4
James Kelly <jhkelly05@gmail.com>
2024-02-19 18:06:43 +0000

    Add properties application

--- main.tf
+++ main.tf
@@ -7,8 +7,6 @@ resource "azurerm_resource_group" "blue-green-rg" {
   location = "UK West"
 }
 
-# SKU.","Target":null,"Details":[{"Message":"Cannot complete the operation because the site will exceed the number of slots allowed for the 'Basic'
-#sku_name= "B1"
 resource "azurerm_service_plan" "blue-green-service-plan" {
   name                = "blue-green-appservice-plan"
   location            = azurerm_resource_group.blue-green-rg.location
@@ -17,8 +15,8 @@ resource "azurerm_service_plan" "blue-green-service-plan" {
   sku_name            = "S1"
 }
 
-resource "azurerm_linux_web_app" "blue-green-service" {
-  name                = "blue-green-appservice"
+resource "azurerm_linux_web_app" "blue-green-properties-appservice" {
+  name                = "blue-green-properties-appservice"
   resource_group_name = azurerm_resource_group.blue-green-rg.name
   location            = azurerm_service_plan.blue-green-service-plan.location
   service_plan_id     = azurerm_service_plan.blue-green-service-plan.id
@@ -33,16 +31,44 @@ resource "azurerm_linux_web_app" "blue-green-service" {
   }
 }
 
-resource "azurerm_linux_web_app_slot" "blue-green-staging-slot" {
-  name                = "staging"
-  app_service_id      = azurerm_linux_web_app.blue-green-service.id
+resource "azurerm_linux_web_app_slot" "blue-green-properties-staging-slot" {
+  name           = "staging"
+  app_service_id = azurerm_linux_web_app.blue-green-properties-appservice.id
   site_config {
-    health_check_path   = "/actuator/health"
+    health_check_path = "/actuator/health"
     application_stack {
-      java_server = "JAVA"
-      java_version = "17"
+      java_server         = "JAVA"
+      java_version        = "17"
       java_server_version = "17"
     }
   }
+}
 
+  resource "azurerm_linux_web_app" "blue-green-appservice" {
+    name                = "blue-green-appservice"
+    resource_group_name = azurerm_resource_group.blue-green-rg.name
+    location            = azurerm_service_plan.blue-green-service-plan.location
+    service_plan_id     = azurerm_service_plan.blue-green-service-plan.id
+
+    site_config {
+      health_check_path   = "/actuator/health"
+      application_stack {
+        java_server = "JAVA"
+        java_version = "17"
+        java_server_version = "17"
+      }
+    }
+  }
+
+  resource "azurerm_linux_web_app_slot" "blue-green-staging-slot" {
+    name                = "staging"
+    app_service_id      = azurerm_linux_web_app.blue-green-appservice.id
+    site_config {
+      health_check_path   = "/actuator/health"
+      application_stack {
+        java_server = "JAVA"
+        java_version = "17"
+        java_server_version = "17"
+      }
+    }
 }
